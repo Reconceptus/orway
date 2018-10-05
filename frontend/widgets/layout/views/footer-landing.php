0d3f2900f524db811f23a36c58d0eeb54ffa7e1c
suhov-as <suhov.a.s@yandex.ru>
2018-10-05 09:35:58 +0300

    фикс футера и ошибки с массивом тегов

---
+++ frontend/widgets/layout/views/footer-landing.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: borod
+ * Date: 05.10.2018
+ * Time: 9:32
+ */
+?>
+<footer class="footer">
+    <div class="content content--lg">
+        <div class="footer--main">
+            <?= \frontend\widgets\pages\Pages::widget() ?>
+            <nav class="nav">
+                <?= $this->render('_menu') ?>
+                <div class="request">
+                    <button class="btn btn--dark show-modal" data-modal="request" type="button">request</button>
+                </div>
+            </nav>
+        </div>
+    </div>
+</footer>
+</section>
+</div>
